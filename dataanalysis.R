81ded5eb2a844c739c1f032e932f6f6c0283b550
CherWeiYuan <47816698+CherWeiYuan@users.noreply.github.com>
2019-10-29 21:34:36 +0800

    Add files via upload

---
+++ dataanalysis.R
@@ -0,0 +1,88 @@
+setwd('D:/OneDrive/Modules/Y4S1 (Current)/SP3203 Aquatic Ecology Research/My Manuscript')
+data <- read.csv('data.csv', header = T)
+
+#Check data is loaded correctly
+data
+View(data)
+summary(data)
+
+#check species is categorical data
+data$species
+
+#linear regression model
+model1 <- lm(mp_ingested_adjusted ~ height * width * weight * species, data = data)
+summary(model1)
+par(mfrow = c(2,2))
+plot(model1)
+#Heteroscedasticity detected in residuals against fitted plot. 
+
+#Breusch-Pagan test for heteroscedasticity
+library(lmtest)
+bptest(model1)
+#no heteroscedasticity present
+
+#identifying outliers
+cooksd <- cooks.distance(model1)
+sample_size <- nrow(data)
+plot(cooksd, pch="*", cex=2, main="Influential Obs by Cooks distance")  # plot cook's distance
+abline(h = 4/sample_size, col="red")  # add cutoff line
+text(x=1:length(cooksd)+1, y=cooksd, labels=ifelse(cooksd>4/sample_size, names(cooksd),""), col="red")  # add labels
+
+#All identified points are outliers due to the relatively higher amount of microplastic ingested
+#may be biologically relevant, so we cannot remove them.
+
+#checking for multicollinearity
+library(car)
+vif(model1)
+#collinearity observed, most likely from height and width.
+#The mussels would be considered very large if length was used, even if they have low mass.
+#Height, is then a reasonable choice because it scales with bivalve size well across the three species.
+model2 <- lm(mp_ingested_adjusted ~ height * weight * species, data = data)
+vif(model2)
+#collinearity issue persist
+#Weight is a proxy to measure the mass, hence size of the bivalves. But wet weight is confounded by water content.
+#Thus, it is not an idea measure of bivalve size.
+model3 <- lm(mp_ingested_adjusted ~ height * species, data = data)
+vif(model3)
+#although score for all variables are above 3, they are already the minimum number of variables I can include in the model
+#therefore, not dropping any
+
+#listing down all possible models for AIC
+model4 <- lm(mp_ingested_adjusted ~ height * species, data = data)
+model5 <- lm(mp_ingested_adjusted ~ height + species, data = data)
+model6 <- lm(mp_ingested_adjusted ~ height, data = data)
+model7 <- lm(mp_ingested_adjusted ~ species, data = data)
+
+#since sample size is large (n = 48), use AIC instead of AICc
+library(MuMIn)
+model_sel <- model.sel(model4, model5, model6, model7, rank = 'AIC')
+model_sel
+
+#model7 has the lowest AIC but has almost identical AIC values as model4 (0.1 difference)
+#but model4 models height and its interaction with species, thus is a comprehensive model
+#so model4 is my chosen model.
+
+#analyzing the model
+model4 <- lm(mp_ingested_adjusted ~ height * species, data = data)
+summary(model4)
+
+library(ggplot2)
+myPlot <- ggplot(data, aes(height, mp_ingested_adjusted, colour = species)) +
+  geom_smooth(method="lm", size = 1.2, se = FALSE) +
+  geom_point(size=3) +
+  theme_bw() + 
+  xlab("Height (mm)") +
+  ylab("Microplastic ingested (mg)") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        legend.title = element_text(face = "italic")) + 
+  scale_y_continuous(limits=c(0, 60))+
+  scale_color_discrete(name ="Species:", 
+                       labels=c("M. meretrix", "M. lusoria")) +
+  scale_shape_discrete(guide = guide_legend(label.theme = element_text(angle = 0, face = "italic")))
+  theme_classic() 
+myPlot
+
+ggsave(myPlot, filename = "regressionPlot.png", dpi = 300, type = "cairo",
+       width = 6, height = 4, units = "in")
+
+
--- poweranalysis.R
+++ poweranalysis.R
@@ -1,8 +1,7 @@
-install.packages("pwr")
 library(pwr)
 
 #Our goal: mass of particles absorbed ~ species * particle_type * dry_mass
-numVariables = 3
+numVariables = 2
 
 #Cohen suggests f2 values of 0.02, 0.15, and 0.35 represent small, medium, and large effect sizes.
 pwr.f2.test(u=numVariables, v=NULL, f2=0.02, sig.level = 0.05, power = 0.8) #low effect size
@@ -11,26 +10,42 @@ pwr.f2.test(u=numVariables, v=NULL, f2=0.35, sig.level = 0.05, power = 0.8) #hig
 
 #Results:
 
-#If numVariables = 3
+#If numVariables = 2
 #sample size = v+u+1
-#low effect size: sample size = 546 + 3 + 1 = 550
-#med effect size: sample size = 72 + 3 + 1 = 76
-#high effect size: sample size = 31 + 3 + 1 = 35
+#low effect size: sample size = 482 + 2 + 1 = 485
+#med effect size: sample size = 64 + 2 + 1 = 67
+#high effect size: sample size = 27 + 2 + 1 = 30
 
 #i.e. if effect size is high, we need 35 observations, or 35 tanks of 3 bivalves each. Need 105 bivalves.
 #if effect size is med, we need 76 * 3 = 228 bivalves in 76 tanks. 
 
-install.packages("WebPower")
+#Since we have a sample size of 48, the power we have:
+
 library(WebPower)
 
 #Our goal: mass of particles absorbed ~ species * particle_type * dry_mass
-numVariables = 3
+numVariables = 2
 
 #For medium effect size:
 wp.regression(n = NULL, p1 = numVariables, p2 = 0, f2 = 0.15, alpha = 0.05, power = 0.8) 
-#We need sample size of 85
+#We need sample size of 67
+
+#For large effect size:
+wp.regression(n = NULL, p1 = numVariables, p2 = 0, f2 = 0.35, alpha = 0.05, power = 0.8) 
+#We need sample size of 30
 
 #To generate a power curve given a sequence of sample sizes:
 res <- wp.regression(n = seq(10,100,10), p1 = numVariables, f2 = 0.15, alpha = 0.05, power = NULL)
 res
 plot(res)
+
+#Since we have a sample size of 48, the power we have:
+#For small effect size:
+wp.regression(n = 48, p1 = numVariables, p2 = 0, f2 = 0.02, alpha = 0.05, power = NULL) 
+#For medium effect size:
+wp.regression(n = 48, p1 = numVariables, p2 = 0, f2 = 0.15, alpha = 0.05, power = NULL) 
+#For large effect size:
+wp.regression(n = 48, p1 = numVariables, p2 = 0, f2 = 0.35, alpha = 0.05, power = NULL) 
+#We need sample size of 30
+
+#power for small is 0.12, medium is 0.64 and large is 0.95
